9c1131f2961ca84794422e63aae081dd6c7d3697
ricdil128 <riccardodiluca@gmail.com>
2025-03-17 18:29:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/energy-bill-splitter/" : "/",  // AGGIUNTA BASE URL PER GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
